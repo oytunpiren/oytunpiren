15124bc7d7b811d1650b2831defab747b38b14f4
oytun <oytunpiren@gmail.com>
2019-12-05 15:40:48 -0600

    commit

---
+++ src/gitPractice/firstProject.java
@@ -0,0 +1,14 @@
+package gitPractice;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class firstProject {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 5;
+    }
+
+
+}
